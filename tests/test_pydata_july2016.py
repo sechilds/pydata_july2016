defa820139cc1d7c47c5537704287619aea97e04
Stephen Childs <Stephen.Childs@ucalgary.ca>
2016-07-06 16:00:08 -0600

    Fix python path.

--- tests/test_pydata_july2016.py
+++ tests/test_pydata_july2016.py
@@ -1,6 +1,6 @@
 import pytest
 from pydata_july2016 import pydata_july2016
 
-def test_get_python_path():
-    assert python in pydata_july2016.get_python_path()
+def test_python_path():
+    assert python in pydata_july2016.python_path()
 
