8ce7384565e729a101a2d852fc8bf83e0f5cd5bc
Stephen Childs <Stephen.Childs@ucalgary.ca>
2016-07-06 16:03:02 -0600

    Add a test for site packages.

--- tests/test_pydata_july2016.py
+++ tests/test_pydata_july2016.py
@@ -4,3 +4,6 @@ from pydata_july2016 import pydata_july2016
 def test_python_path():
     assert 'python' in pydata_july2016.python_path()
 
+def test_site_packages():
+    assert 'site-packages' in pydata_july2016.python_path()
+
