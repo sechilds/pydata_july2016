d0f04b6ffd4451ad63e64f2036873bbd9bd00166
Stephen Childs <Stephen.Childs@ucalgary.ca>
2016-07-06 16:01:25 -0600

    Add test.

--- tests/test_pydata_july2016.py
+++ tests/test_pydata_july2016.py
@@ -2,5 +2,5 @@ import pytest
 from pydata_july2016 import pydata_july2016
 
 def test_python_path():
-    assert python in pydata_july2016.python_path()
+    assert 'python' in pydata_july2016.python_path()
 
